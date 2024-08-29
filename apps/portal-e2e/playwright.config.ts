a8b701f4d91be3bc70c1c8323fff73c0208d035d
Rich Tillman <richtillman@gmail.com>
2024-08-29 13:57:37 -0400

    chore: nx migrate/update to 19.6.4

--- apps/portal-e2e/playwright.config.ts
+++ apps/portal-e2e/playwright.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test'
+import { nxE2EPreset } from '@nx/playwright/preset'
 
-import { workspaceRoot } from '@nx/devkit';
+import { workspaceRoot } from '@nx/devkit'
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4300'
 
 /**
  * Read environment variables from file.
@@ -21,30 +21,30 @@ export default defineConfig({
   use: {
     baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx serve portal',
-    url: 'http://localhost:4200',
+    command: 'npx nx run portal:preview',
+    url: 'http://localhost:4300',
     reuseExistingServer: !process.env.CI,
-    cwd: workspaceRoot,
+    cwd: workspaceRoot
   },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'] }
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+      use: { ...devices['Desktop Safari'] }
+    }
 
     // Uncomment for mobile browsers support
     /* {
@@ -65,5 +65,5 @@ export default defineConfig({
       name: 'Google Chrome',
       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     } */
-  ],
-});
+  ]
+})
